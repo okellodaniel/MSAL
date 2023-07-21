3c27692f905aa60160f00d6fa0413b6500a180ac
Daniel Okello <43291086+okellodaniel@users.noreply.github.com>
2023-07-21 23:18:21 +0300

    finished

--- Program.cs
+++ Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿using System.Threading.Tasks;
+using Microsoft.Identity.Client;
+public class Program
+{
+    private const string _clientId = "0db441c0-d94d-4a40-b1ea-6542daf50ec7";
+    private const string _tenantId = "b16ec749-3645-45f9-b332-43afb8d8db16";
+    private static async Task Main(string[] args)
+    {
+        var app = PublicClientApplicationBuilder
+        .Create(_clientId)
+        .WithAuthority(AzureCloudInstance.AzurePublic, _tenantId)
+        .WithRedirectUri("http://localhost")
+        .Build();
+
+        string[] scopes = { "user.read" };
+
+        var result = await app.AcquireTokenInteractive(scopes).ExecuteAsync();
+
+        Console.WriteLine("Token : \t {0}", result.AccessToken);
+    }
+}
